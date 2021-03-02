26afd5fa5de46da5133e6457c309dbbbe15d98b7
Fernando <costasfernando199@gmail.com>
2021-03-01 22:42:19 -0300

    configure EsLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,36 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '*.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-extra-parens': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'arrow-parens': 'off',
+    eqeqeq: 'error',
+    'function-paren-newline': 'off',
+    'linebreak-style': [2, 'unix'],
+    'no-console': [
+      'error',
+      {
+        allow: ['info', 'warn', 'error', 'time', 'timeEnd'],
+      },
+    ],
+    'no-duplicate-imports': 'error',
+    'no-extra-parens': 'error',
+    'no-return-await': 'error',
+    'no-shadow': [
+      'error',
+      {
+        builtinGlobals: false,
+        hoist: 'functions',
+        allow: [],
+      },
+    ],
+    'operator-linebreak': [2, 'before', { overrides: { '?': 'after' } }],
+    'import/prefer-default-export': 'off',
   },
 };
